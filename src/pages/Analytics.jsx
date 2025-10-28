1b003451b1e582991b4ad04874f2028aded017dd
Chris Rogers <chris@starsite.digital>
2025-10-28 09:28:37 +0000

    Fix syntax error in Analytics.jsx filter section

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -523,8 +523,6 @@ const Analytics = () => {
               </div>
 
               {showFilters && (
-                <div>
-
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-4">
                 {/* Search */}
                 <div className="lg:col-span-2">
