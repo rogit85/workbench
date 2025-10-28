eb84f73b2e0f1683028d187a4e8d05a2d0548540
Chris Rogers <chris@starsite.digital>
2025-10-28 09:28:11 +0000

    Fix syntax error in AIAccuracy.jsx filter section

--- src/pages/AIAccuracy.jsx
+++ src/pages/AIAccuracy.jsx
@@ -227,8 +227,6 @@ const AIAccuracy = () => {
               </div>
 
               {showFilters && (
-                <div>
-
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
                 {/* LoB */}
                 <div>
